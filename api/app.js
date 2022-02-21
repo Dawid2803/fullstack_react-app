d61eba5298ace52d4cba98a01036db2079e4b032
Dawid2803 <dawidjacoba@gmail.com>
2022-02-21 20:18:45 +0200

    successfully implemented CORS

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@
 // load modules
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const routes = require('./routes');
 const { sequelize } = require('./models');
 
@@ -11,6 +12,9 @@ const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'tr
 
 // create the Express app
 const app = express();
+
+// Enable all CORS Requests
+app.use(cors());
 
 // setup morgan which gives us http request logging
 app.use(morgan('dev'));

17561df3c3df4b71ca65f35c4b1e31aae4cdf469
Dawid2803 <dawidjacoba@gmail.com>
2022-03-02 19:25:51 +0200

    fixed outstanding CourseDetail issues

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
-//import ReactMarkdown from 'react-markdown';
+import ReactMarkdown from 'react-markdown';
+import reactMarkdown from 'react-markdown';
 
 const CourseDetail = (props) => {
   const {context} = props;
@@ -32,14 +33,14 @@ const CourseDetail = (props) => {
         <div>
           <h3 className='course--detail--title'>Course</h3>
           <h4 className='course--name'>{courseDetails.title}</h4>
-          <p>By {courseDetails.User.firstName} {courseDetails.User.lastName}</p>
-          <p>{courseDetails.description}</p>
+          <p>By {courseCreator.firstName} {courseCreator.lastName}</p>
+          <ReactMarkdown>{courseDetails.description}</ReactMarkdown>
         </div>
         <div>
           <h3 className='course--detail--title'>Materials Needed</h3>
-          <ul className='course-detail--list'>
-            <li>{courseDetails.materialsNeeded}</li>
-          </ul>
+          <ReactMarkdown className='course-detail--list'>
+            {courseDetails.materialsNeeded}
+          </ReactMarkdown>
         </div>
       </div>
     </form>
